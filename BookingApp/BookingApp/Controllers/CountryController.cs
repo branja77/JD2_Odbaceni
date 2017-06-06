48000d947c18a8191209324279b45c911b280939
branja7 <branko.savic94@gmail.com>
2017-06-06 12:54:31 +0200

    Napravljen kontroler za Country

---
+++ BookingApp/BookingApp/Controllers/CountryController.cs
@@ -0,0 +1,94 @@
+﻿using BookingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BookingApp.Controllers
+{
+    public class CountryController : ApiController
+    {
+        private static BAContext context = new BAContext();
+        public IHttpActionResult Get()
+        {
+            return Json(context.Countries);
+        }
+
+        // GET api/values/5
+        public IHttpActionResult Get(int id)
+        {
+            var country = context.Countries.Find(id);
+            if(country != null)
+            {
+                return Ok(country);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // POST api/values
+        public IHttpActionResult Post([FromBody]Country country)
+        {
+            try
+            {
+                context.Countries.Add(country);
+                context.SaveChanges();
+                return CreatedAtRoute("DefaultApi", new { id = country.Id }, country);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return BadRequest();
+            }
+        }
+
+        // PUT api/values/5
+        public IHttpActionResult Put(int id, [FromBody]Country country)
+        {
+            var foundCountry = context.Countries.Find(id);
+            context.Entry(country).State = EntityState.Modified;
+            try
+            {
+                context.SaveChanges();
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                if (foundCountry == null)
+                    return NotFound();
+                return BadRequest();
+            }
+
+        }
+
+        // DELETE api/values/5
+        public IHttpActionResult Delete(int id)
+        {
+            var country = context.Countries.Find(id);
+            if(country != null)
+            {
+                context.Countries.Remove(country);
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return StatusCode(HttpStatusCode.InternalServerError);
+                }
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ BookingApp/BookingApp/Migrations/201706061029213_DeployBookingDatabase.cs
@@ -0,0 +1,166 @@
+namespace BookingApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeployBookingDatabase : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Accommodations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                        Address = c.String(),
+                        AvrageGrade = c.Double(nullable: false),
+                        Latitude = c.Double(nullable: false),
+                        Longitude = c.Double(nullable: false),
+                        ImageURL = c.String(),
+                        Approved = c.Boolean(nullable: false),
+                        AccomodationType_Id = c.Int(nullable: false),
+                        Owner_Id = c.Int(nullable: false),
+                        Place_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AccommodationTypes", t => t.AccomodationType_Id, cascadeDelete: false)
+                .ForeignKey("dbo.AppUsers", t => t.Owner_Id, cascadeDelete: false)
+                .ForeignKey("dbo.Places", t => t.Place_Id, cascadeDelete: false)
+                .Index(t => t.AccomodationType_Id)
+                .Index(t => t.Owner_Id)
+                .Index(t => t.Place_Id);
+            
+            CreateTable(
+                "dbo.AccommodationTypes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Comments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Grade = c.Int(nullable: false),
+                        Text = c.String(),
+                        Accomodation_Id = c.Int(nullable: false),
+                        User_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Accommodations", t => t.Accomodation_Id, cascadeDelete: false)
+                .ForeignKey("dbo.AppUsers", t => t.User_Id, cascadeDelete: false)
+                .Index(t => t.Accomodation_Id)
+                .Index(t => t.User_Id);
+            
+            CreateTable(
+                "dbo.RoomReservations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        StartDate = c.DateTime(nullable: false),
+                        EndDate = c.DateTime(nullable: false),
+                        Timestamp = c.DateTime(nullable: false),
+                        Room_Id = c.Int(nullable: false),
+                        User_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Rooms", t => t.Room_Id, cascadeDelete: false)
+                .ForeignKey("dbo.AppUsers", t => t.User_Id, cascadeDelete: false)
+                .Index(t => t.Room_Id)
+                .Index(t => t.User_Id);
+            
+            CreateTable(
+                "dbo.Rooms",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        RoomNumber = c.Int(nullable: false),
+                        BedCount = c.Int(nullable: false),
+                        Description = c.String(),
+                        PricePerNight = c.Double(nullable: false),
+                        Accomodation_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Accommodations", t => t.Accomodation_Id, cascadeDelete: false)
+                .Index(t => t.Accomodation_Id);
+            
+            CreateTable(
+                "dbo.Places",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Region_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Regions", t => t.Region_Id, cascadeDelete: false)
+                .Index(t => t.Region_Id);
+            
+            CreateTable(
+                "dbo.Regions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Country_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Countries", t => t.Country_Id, cascadeDelete: false)
+                .Index(t => t.Country_Id);
+            
+            CreateTable(
+                "dbo.Countries",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Code = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.AppUsers", "Username", c => c.String());
+            AddColumn("dbo.AppUsers", "Email", c => c.String());
+            AddColumn("dbo.AppUsers", "Password", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Accommodations", "Place_Id", "dbo.Places");
+            DropForeignKey("dbo.Places", "Region_Id", "dbo.Regions");
+            DropForeignKey("dbo.Regions", "Country_Id", "dbo.Countries");
+            DropForeignKey("dbo.Accommodations", "Owner_Id", "dbo.AppUsers");
+            DropForeignKey("dbo.Comments", "User_Id", "dbo.AppUsers");
+            DropForeignKey("dbo.RoomReservations", "User_Id", "dbo.AppUsers");
+            DropForeignKey("dbo.RoomReservations", "Room_Id", "dbo.Rooms");
+            DropForeignKey("dbo.Rooms", "Accomodation_Id", "dbo.Accommodations");
+            DropForeignKey("dbo.Comments", "Accomodation_Id", "dbo.Accommodations");
+            DropForeignKey("dbo.Accommodations", "AccomodationType_Id", "dbo.AccommodationTypes");
+            DropIndex("dbo.Regions", new[] { "Country_Id" });
+            DropIndex("dbo.Places", new[] { "Region_Id" });
+            DropIndex("dbo.Rooms", new[] { "Accomodation_Id" });
+            DropIndex("dbo.RoomReservations", new[] { "User_Id" });
+            DropIndex("dbo.RoomReservations", new[] { "Room_Id" });
+            DropIndex("dbo.Comments", new[] { "User_Id" });
+            DropIndex("dbo.Comments", new[] { "Accomodation_Id" });
+            DropIndex("dbo.Accommodations", new[] { "Place_Id" });
+            DropIndex("dbo.Accommodations", new[] { "Owner_Id" });
+            DropIndex("dbo.Accommodations", new[] { "AccomodationType_Id" });
+            DropColumn("dbo.AppUsers", "Password");
+            DropColumn("dbo.AppUsers", "Email");
+            DropColumn("dbo.AppUsers", "Username");
+            DropTable("dbo.Countries");
+            DropTable("dbo.Regions");
+            DropTable("dbo.Places");
+            DropTable("dbo.Rooms");
+            DropTable("dbo.RoomReservations");
+            DropTable("dbo.Comments");
+            DropTable("dbo.AccommodationTypes");
+            DropTable("dbo.Accommodations");
+        }
+    }
+}
