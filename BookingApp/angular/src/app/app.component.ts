aae2cf2e33bd300d241da7698bdff62d08d89429
branja7 <branko.savic94@gmail.com>
2017-06-11 17:19:17 +0200

    angular template

--- BookingApp/angular/src/app/app.component.ts
+++ BookingApp/angular/src/app/app.component.ts
@@ -1,23 +1,11 @@
 import { Component } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  ime = 'Branko';
-  prezime = 'Savic';
-  voce = [
-    {naziv: "jabuka", "boja":"zelena"},
-    {naziv: "kruska", "boja":"ljubicasta"},
-    {naziv: "jagoda", "boja":"crvena"}
-  ];
-
- clickedItem = {naziv : "", "boja":""};
-
-   kliknutRed(Item){
-      this.clickedItem = Item;
-  }
 }
--- BookingApp/angular/src/app/app.module.ts
+++ BookingApp/angular/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AccommodationComponent } from './accommodation/accommodation.component';
+
+const Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "accommodation", component: AccommodationComponent},
+  //{path: "", redirectTo:"home"}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AccommodationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(Routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
