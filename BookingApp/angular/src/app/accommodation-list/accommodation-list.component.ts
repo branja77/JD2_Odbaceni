880f29627395b9bd7b758918712aea01337b5773
branja7 <branko.savic94@gmail.com>
2017-06-12 10:43:09 +0200

    dodate nove komponente

---
+++ BookingApp/angular/src/app/accommodation-list/accommodation-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accommodation-list',
+  templateUrl: './accommodation-list.component.html',
+})
+export class AccommodationListComponent {
+
+}
