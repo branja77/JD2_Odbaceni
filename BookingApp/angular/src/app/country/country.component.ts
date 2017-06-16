e85dd708a68105c669ee9ca4c498a1ac85dfb3c5
branja7 <branko.savic94@gmail.com>
2017-06-16 19:26:07 +0200

    izmjena drzava, al ne radi jos kako treba

--- BookingApp/angular/src/app/country/country.component.ts
+++ BookingApp/angular/src/app/country/country.component.ts
@@ -5,14 +5,20 @@ import {
 } from '@angular/router';
 
 import { Country } from '../model/country.model';
+import { HttpCountriesService } from '../services/http-countries.service';
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
 })
 export class CountryComponent {
-  constructor(private router: Router, private activatedRoute: ActivatedRoute)
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private countriesService: HttpCountriesService)
   {
   }
   @Input() country: Country;
+
+  onSubmit(country: Country){
+    debugger
+      //this.countriesService.postCountry(country);
+  }
 }
